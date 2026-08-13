0336b6909fd04fc09df4e41bee956fda26c70a2a
sakai-soroban <sakai.soroban.888@gmail.com>
2026-08-13 20:09:09 +0900

    Update liffId in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ここに置けるのは公開情報だけです。LINE Channel Secretやservice_role keyは禁止です。
 window.SAKAI_CONFIG = Object.freeze({
-  liffId: "2010489123-VOHZechV",
+  liffId: "2010489123-JwYLhw0D",
   lineAuthEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/exchange",
   lineLinkEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/link",
   absenceSubmitEndpoint: "YOUR_HTTPS_API_BASE_URL/functions/v1/submit-absence",
