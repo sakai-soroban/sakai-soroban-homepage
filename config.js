25a5f4fb36cd93b19238bb6c844350a2b0b77045
sakai-soroban <sakai.soroban.888@gmail.com>
2026-08-13 22:35:57 +0900

    Update supabaseUrl in config.js
    Removed unnecessary path from supabaseUrl.

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.SAKAI_CONFIG = Object.freeze({
   lineAuthEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/exchange",
   lineLinkEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/link",
   absenceSubmitEndpoint: "YOUR_HTTPS_API_BASE_URL/functions/v1/submit-absence",
-  supabaseUrl: "＝https://qyljgjqhtkhackyyibwf.supabase.co/rest/v1/",
+  supabaseUrl: "＝https://qyljgjqhtkhackyyibwf.supabase.co",
   supabaseAnonKey: "sb_publishable_PcJrp1FAT3IX_u69hI685g_vo2hdc9p"
 });
