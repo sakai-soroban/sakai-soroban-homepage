aa8fd5ec248b394c10992b9dbaaa03a7b54ac20d
sakai-soroban <sakai.soroban.888@gmail.com>
2026-08-13 20:53:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.SAKAI_CONFIG = Object.freeze({
   lineAuthEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/exchange",
   lineLinkEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/link",
   absenceSubmitEndpoint: "YOUR_HTTPS_API_BASE_URL/functions/v1/submit-absence",
-  supabaseUrl: "YOUR_SUPABASE_PROJECT_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY"
+  supabaseUrl: "＝https://qyljgjqhtkhackyyibwf.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable_PcJrp1FAT3IX_u69hI685g_vo2hdc9p"
 });
