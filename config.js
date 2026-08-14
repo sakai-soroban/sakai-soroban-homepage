356a77b04eee7c300a8baca8d166728bcb001c82
sakai-soroban <sakai.soroban.888@gmail.com>
2026-08-14 22:20:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // ここに置けるのは公開情報だけです。LINE Channel Secretやservice_role keyは禁止です。
 window.SAKAI_CONFIG = Object.freeze({
   liffId: "2011111788-nxQ1uT13",
-  lineAuthEndpoint: "YOUR_HTTPS_API_BASE_URL/auth/line/exchange",
+  lineAuthEndpoint: "https://qyljgjqhtkhackyyibwf.supabase.co/functions/v1/line-exchange",
   lineLinkEndpoint: "https://qyljgjqhtkhackyyibwf.supabase.co/functions/v1/line-exchange",
   absenceSubmitEndpoint: "YOUR_HTTPS_API_BASE_URL/functions/v1/submit-absence",
-  supabaseUrl: "＝https://qyljgjqhtkhackyyibwf.supabase.co",
+  supabaseUrl: "https://qyljgjqhtkhackyyibwf.supabase.co",
   supabaseAnonKey: "sb_publishable_PcJrp1FAT3IX_u69hI685g_vo2hdc9p"
 });
