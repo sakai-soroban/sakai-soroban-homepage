5ee06a8426efd2f379c4626e3f683873badcc5cd
sakai-soroban <sakai.soroban.888@gmail.com>
2026-08-25 22:32:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.SAKAI_CONFIG = Object.freeze({
   liffId: "2011111788-nxQ1uT13",
   lineAuthEndpoint: "https://qyljgjqhtkhackyyibwf.supabase.co/functions/v1/line-exchange",
   lineLinkEndpoint: "https://qyljgjqhtkhackyyibwf.supabase.co/functions/v1/line-exchange",
-  absenceSubmitEndpoint: "YOUR_HTTPS_API_BASE_URL/functions/v1/submit-absence",
+  absenceSubmitEndpoint: "https://qyljgjqhtkhackyyibwf.supabase.co/functions/v1/submit-absence",
   supabaseUrl: "https://qyljgjqhtkhackyyibwf.supabase.co",
   supabaseAnonKey: "sb_publishable_PcJrp1FAT3IX_u69hI685g_vo2hdc9p"
 });
